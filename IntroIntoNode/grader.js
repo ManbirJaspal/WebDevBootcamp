8d2d7b60179bbf5206a555f4133c6b1ab7c2d57d
ManbirJaspal <manb.jaspal@gmail.com>
2017-12-22 19:29:57 +0000

    npm packages

--- IntroIntoNode/grader.js
+++ IntroIntoNode/grader.js
@@ -1,4 +1,17 @@
 function average(scores){
     //add all scores together
-    var i;
+    var total = 0;
+    
+    scores.forEach(function(score){
+        total += score;
+    })
+    //Divide by total no. of scores
+    var avg = total/scores.length;
+    
+    return Math.round(avg);
 }
+
+var scores = [90, 98, 89, 100, 100, 86, 94];
+console.log(average(scores));
+var scores1 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+console.log(average(scores1));
