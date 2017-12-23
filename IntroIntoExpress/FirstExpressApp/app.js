06b269d36064f949225f6aee086b812c2150eb6e
ManbirJaspal <manb.jaspal@gmail.com>
2017-12-23 04:38:05 +0000

    Excercise1

--- IntroIntoExpress/FirstExpressApp/app.js
+++ IntroIntoExpress/FirstExpressApp/app.js
@@ -19,6 +19,19 @@ app.get("/cat", function(req, res){
    res.send("MEOW!"); 
 });
 
+app.get("/r/:subredditName", function(req, res){  
+    // anything infront of a colon can be changed in the http request. 
+    //ex: comments will will always be always be comments. but the id,title can change.
+    var subreddit = req.params.subredditName;
+   res.send("WELCOME TO THE " + subreddit.toUpperCase()+ " SUBREDDIT"); 
+});
+
+app.get("*", function(req, res){
+   res.send("YOU ARE A STAR!"); 
+});
+
+
+
 // Tell Express to listen for requests (start server)
 
 app.listen(process.env.PORT, process.env.IP, function(){  //the process.env.PORT
--- MyShop/listProducts.js
+++ MyShop/listProducts.js
@@ -2,5 +2,5 @@ var faker = require('faker');
 
 
 for (var i = 0; i < 10 ; i++){
-console.log(faker.commerce.productName() + " - $" + faker.commerce.price());
+    console.log(faker.commerce.productName() + " - $" + faker.commerce.price());
 }
