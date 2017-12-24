5a704c3cffc4a4c57e2b16526c80494b41b0ee36
Manbir Jaspal <manb.jaspal@gmail.com>
2017-12-24 04:43:13 +0000

    Learned apis

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,11 +1,12 @@
 var request = require("request");
-request("http://www.reddit.com/sdfswdfwf", function(error, response, body){  //and cehck for error, check for response.status, body(3 parameters)
+request("https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(error, response, body){  //and cehck for error, check for response.status, body(3 parametewr)
     if (error){
         console.log("Something has gone wrong!");
     } else {
         if(response.statusCode == 200){
             //Thingsnworked!
-            console.log(body);
+           var parsedData = JSON.parse(body); //  JSON is actually in string format. and what JSON.parse does it that it turns it into an object
+            console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]);
         }
     }
 });
