45759183669b21c44e748f5a75a82407d33df77c
Manbir Jaspal <manb.jaspal@gmail.com>
2017-12-27 23:21:50 +0000

    Finished the RESTful Blog App

--- RESTful_Routing/RESTfulBlogApp/app.js
+++ RESTful_Routing/RESTfulBlogApp/app.js
@@ -1,13 +1,16 @@
 var bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    express     = require("express"),
-    app         = express();
+methodOverride = require("method-override"),
+mongoose        = require("mongoose"),
+express         = require("express"),
+app             = express();
 
 //APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));   //the overridemethod scans the url for anything that _method is equal to(PUT,Delete,GET,POST etc.) and treat it like that request.
+                                        //we use the methodOverride package because html doesnt support PUT and DELETE requests. and this is a tweak around it.
  
  //MONGOOSE.MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -18,8 +21,87 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
+// Blog.create({
+//     title: "Test Blog!",
+//     image: "https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg",
+//     body: "HELLO THIS IS A BLOG POST"
+// });
 //RESTFUL ROUTES
-app.get("/")
+
+app.get("/", function(req, res) {
+   res.redirect("/blogs"); 
+});
+
+//INDEX ROUTE
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("error!!!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+   res.render("new"); 
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else  {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err,foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){    //the parameters for Blog.findByIdAndUpdate is Blog.findByIdAndUpdate(id, newData, callback)
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/"+ req.params.id);
+        }
+    });       
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
